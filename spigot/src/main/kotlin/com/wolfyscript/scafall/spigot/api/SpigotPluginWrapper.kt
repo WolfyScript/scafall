5592a177ae914138333fea0ae7be6192a07d0703
WolfyScript <wolfyscript@gmail.com>
2024-08-19 18:06:25 +0200

    Make PluginWrapper.into public

--- spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/SpigotPluginWrapper.kt
+++ spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/SpigotPluginWrapper.kt
@@ -2,7 +2,6 @@ package com.wolfyscript.scafall.spigot.api
 
 import com.wolfyscript.scafall.PluginWrapper
 import com.wolfyscript.scafall.common.api.into
-import io.netty.util.internal.logging.Slf4JLoggerFactory
 import org.bukkit.plugin.Plugin
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -15,6 +14,6 @@ class SpigotPluginWrapper(val plugin: Plugin) : PluginWrapper {
 
 }
 
-internal fun PluginWrapper.into() : SpigotPluginWrapper {
+fun PluginWrapper.into() : SpigotPluginWrapper {
     return into<SpigotPluginWrapper>()
 }

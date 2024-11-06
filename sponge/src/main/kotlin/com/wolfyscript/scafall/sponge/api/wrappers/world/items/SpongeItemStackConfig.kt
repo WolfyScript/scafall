2cbe4b72d9552701110a7453e83a4b19bbc814f6
WolfyScript <wolfyscript@gmail.com>
2024-11-06 19:33:27 +0100

    Apply DataKeys upon ItemStack construction (Sponge)

--- sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/world/items/SpongeItemStackConfig.kt
+++ sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/world/items/SpongeItemStackConfig.kt
@@ -1,5 +1,7 @@
 package com.wolfyscript.scafall.sponge.api.wrappers.world.items
 
+import com.wolfyscript.scafall.common.api.data.SnapshotDataComponentMap
+import com.wolfyscript.scafall.data.DataKey
 import com.wolfyscript.scafall.eval.context.EvalContext
 import com.wolfyscript.scafall.sponge.api.wrappers.wrap
 import com.wolfyscript.scafall.wrappers.world.items.ItemStackConfig
@@ -11,15 +13,28 @@ import org.spongepowered.api.item.ItemTypes
 import org.spongepowered.api.item.inventory.ItemStack
 import kotlin.jvm.optionals.getOrNull
 
-class SpongeItemStackConfig(itemId: String) : ItemStackConfig(itemId) {
+class SpongeItemStackConfig(itemId: String) : ItemStackConfig(itemId, SnapshotDataComponentMap()) {
 
     override fun constructItemStack(
         context: EvalContext,
         miniMessage: MiniMessage?,
         tagResolvers: TagResolver
-    ): com.wolfyscript.scafall.wrappers.world.items.ItemStack? {
+    ): com.wolfyscript.scafall.wrappers.world.items.ItemStack {
         val spongeStack : ItemStack = ItemTypes.registry().findValue<ItemType>(ResourceKey.resolve(itemId)).getOrNull()?.let { ItemStack.of(it) } ?: ItemStack.empty()
 
-        return spongeStack.wrap()
+        val wrappedStack = spongeStack.wrap()
+
+        for (dataKey in data().keys()) {
+            applyDataKey(wrappedStack, dataKey)
+        }
+
+        return wrappedStack
     }
+
+    private fun <T: Any> applyDataKey(stack: com.wolfyscript.scafall.wrappers.world.items.ItemStack, dataKey: DataKey<T, com.wolfyscript.scafall.wrappers.world.items.ItemStack>) {
+        data().get(dataKey)?.let {
+            dataKey.writeTo(it, stack)
+        }
+    }
+
 }

16fe387ccc0611b84aafb2cf12333056c54410e7
WolfyScript <wolfyscript@gmail.com>
2024-11-06 19:30:22 +0100

    Add unwrap ext for ItemStacks (Sponge)

--- sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/SpongeWrapper.kt
+++ sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/SpongeWrapper.kt
@@ -6,3 +6,7 @@ import org.spongepowered.api.item.inventory.ItemStack
 fun ItemStack.wrap() : com.wolfyscript.scafall.wrappers.world.items.ItemStack {
     return ItemStackWrapper(this)
 }
+
+fun com.wolfyscript.scafall.wrappers.world.items.ItemStack.unwrap(): ItemStack {
+    return (this as ItemStackWrapper).ref
+}

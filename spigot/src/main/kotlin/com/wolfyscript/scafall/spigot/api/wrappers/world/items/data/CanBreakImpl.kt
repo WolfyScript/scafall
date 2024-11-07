627991ec225dda54865f52c2e9e3c201d899d66c
WolfyScript <wolfyscript@gmail.com>
2024-11-07 19:19:21 +0100

    Mention data loss of can_break and can_place (Spigot)

--- spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/wrappers/world/items/data/CanBreakImpl.kt
+++ spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/wrappers/world/items/data/CanBreakImpl.kt
@@ -3,28 +3,32 @@ package com.wolfyscript.scafall.spigot.api.wrappers.world.items.data
 import com.wolfyscript.scafall.ScafallProvider
 import com.wolfyscript.scafall.identifier.Key
 import com.wolfyscript.scafall.platform.PlatformType
-import com.wolfyscript.scafall.spigot.api.identifiers.bukkit
 import com.wolfyscript.scafall.spigot.api.identifiers.api
+import com.wolfyscript.scafall.spigot.api.identifiers.bukkit
 import com.wolfyscript.scafall.wrappers.world.items.data.CanBreak
 import org.bukkit.Material
 import org.bukkit.inventory.ItemFlag
 
-internal val canBreakItemMetaConverter = ItemMetaDataKeyConverter<CanBreak>({
-    val show = !hasItemFlag(ItemFlag.HIDE_PLACED_ON)
-    return@ItemMetaDataKeyConverter if (ScafallProvider.get().platformType == PlatformType.PAPER) {
-        CanBreak(show, destroyableKeys.map { Key.key(it.namespace, it.key) })
-    } else {
-        CanBreak(show, canDestroy.map { it.key.api() })
-    }
-}, { canBreak ->
+internal val canBreakItemMetaConverter =
     if (ScafallProvider.get().platformType == PlatformType.PAPER) {
-        setDestroyableKeys(canBreak.blocks.map { it.bukkit() })
+        // POSSIBLE DATA LOSS: This does set/return the complete data as mentioned here https://github.com/PaperMC/Paper/issues/10818
+        // e.g. can_place/can_destroy can match data components using predicates
+        ItemMetaDataKeyConverter<CanBreak>({
+            val show = !hasItemFlag(ItemFlag.HIDE_PLACED_ON)
+            CanBreak(show, destroyableKeys.map { Key.key(it.namespace, it.key) })
+        }, { canBreak ->
+            setDestroyableKeys(canBreak.blocks.map { it.bukkit() })
+        })
     } else {
-        /*
-        * WARNING: Possible LOSS of Information!
-        *
-        * Keys that are not valid materials are lost!
-        * */
-        canDestroy = canBreak.blocks.map { Material.getMaterial(it.toString()) }.toSet()
+        ItemMetaDataKeyConverter<CanBreak>({
+            val show = !hasItemFlag(ItemFlag.HIDE_PLACED_ON)
+            CanBreak(show, canDestroy.map { it.key.api() })
+        }, { canBreak ->
+            /*
+            * WARNING: Possible LOSS of Information!
+            *
+            * Keys that are not valid materials are lost!
+            * */
+            canDestroy = canBreak.blocks.map { Material.getMaterial(it.toString()) }.toSet()
+        })
     }
-})
--- spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/wrappers/world/items/data/CanPlaceOnImpl.kt
+++ spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/wrappers/world/items/data/CanPlaceOnImpl.kt
@@ -9,22 +9,27 @@ import com.wolfyscript.scafall.wrappers.world.items.data.CanPlaceOn
 import org.bukkit.Material
 import org.bukkit.inventory.ItemFlag
 
-internal val canPlaceOnItemMetaConverter = ItemMetaDataKeyConverter<CanPlaceOn>({
-    val show = !hasItemFlag(ItemFlag.HIDE_PLACED_ON)
-    return@ItemMetaDataKeyConverter if (ScafallProvider.get().platformType == PlatformType.PAPER) {
-        CanPlaceOn(show, placeableKeys.map { Key.key(it.namespace, it.key) })
-    } else {
-        CanPlaceOn(show, canPlaceOn.map { it.key.api() })
-    }
-}, { placeOn ->
+internal val canPlaceOnItemMetaConverter =
     if (ScafallProvider.get().platformType == PlatformType.PAPER) {
-        setPlaceableKeys(placeOn.blocks.map { it.bukkit() })
+        // POSSIBLE DATA LOSS: This does set/return the complete data as mentioned here https://github.com/PaperMC/Paper/issues/10818
+        // e.g. can_place/can_destroy can match data components using predicates
+        ItemMetaDataKeyConverter<CanPlaceOn>({
+            val show = !hasItemFlag(ItemFlag.HIDE_PLACED_ON)
+            CanPlaceOn(show, placeableKeys.map { Key.key(it.namespace, it.key) })
+        }, { placeOn ->
+            setPlaceableKeys(placeOn.blocks.map { it.bukkit() })
+        })
     } else {
-        /*
-        * WARNING: Possible LOSS of Information!
-        *
-        * Keys that are not valid materials are lost!
-        * */
-        canPlaceOn = placeOn.blocks.map { Material.getMaterial(it.toString()) }.toSet()
+        ItemMetaDataKeyConverter<CanPlaceOn>({
+            val show = !hasItemFlag(ItemFlag.HIDE_PLACED_ON)
+            CanPlaceOn(show, canPlaceOn.map { it.key.api() })
+        }, { placeOn ->
+                /*
+                * WARNING: Possible LOSS of Information!
+                *
+                * Keys that are not valid materials are lost!
+                * */
+                canPlaceOn = placeOn.blocks.map { Material.getMaterial(it.toString()) }.toSet()
+        })
     }
-})
+

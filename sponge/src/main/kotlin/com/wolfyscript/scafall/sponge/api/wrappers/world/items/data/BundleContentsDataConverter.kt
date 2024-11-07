bf36b82616da3f75c9b80d683a2f16595f8cda68
WolfyScript <wolfyscript@gmail.com>
2024-11-07 19:42:42 +0100

    How do we get bundle contents in Sponge?

--- sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/world/items/data/BundleContentsDataConverter.kt
+++ sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/world/items/data/BundleContentsDataConverter.kt
@@ -3,4 +3,9 @@ package com.wolfyscript.scafall.sponge.api.wrappers.world.items.data
 import com.wolfyscript.scafall.sponge.api.data.ItemStackDataKeyConverter
 import com.wolfyscript.scafall.wrappers.world.items.data.BundleContents
 
-internal val bundleContentsDataConverter = ItemStackDataKeyConverter<BundleContents>({ TODO("Not implemented yet!")}, { TODO("Not implemented yet!") })
+internal val bundleContentsDataConverter = ItemStackDataKeyConverter<BundleContents>({
+    // TODO: What do we use here to get the items? Keys.INVENTORY maybe?
+    TODO("Not implemented yet!")
+}, {
+    TODO("Not implemented yet!")
+})

17f4f2a28a4ac8c3fa4b042299467801b9abb470
WolfyScript <wolfyscript@gmail.com>
2024-11-08 23:03:37 +0100

    Implement snapshot creation (Sponge)

--- sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/world/items/ItemStackWrapper.kt
+++ sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/world/items/ItemStackWrapper.kt
@@ -4,22 +4,21 @@ import com.wolfyscript.scafall.common.api.data.CommonDataComponentMap
 import com.wolfyscript.scafall.data.DataComponentMap
 import com.wolfyscript.scafall.identifier.Key
 import com.wolfyscript.scafall.sponge.api.wrappers.SpongeRefWrapper
+import com.wolfyscript.scafall.sponge.api.wrappers.wrap
 import com.wolfyscript.scafall.toAPI
 import com.wolfyscript.scafall.wrappers.world.items.ItemStack
-import com.wolfyscript.scafall.wrappers.world.items.ItemStackConfig
+import com.wolfyscript.scafall.wrappers.world.items.ItemStackSnapshot
 import org.spongepowered.api.item.ItemTypes
 
 class ItemStackWrapper(ref: org.spongepowered.api.item.inventory.ItemStack) : SpongeRefWrapper<org.spongepowered.api.item.inventory.ItemStack>(ref), ItemStack {
 
     private val componentMap = CommonDataComponentMap(this)
 
-    override val item: Key
-        get() = ItemTypes.registry().valueKey(ref.type()).toAPI()
-    override val amount: Int
-        get() = ref.quantity()
+    override val item: Key = ItemTypes.registry().valueKey(ref.type()).toAPI()
+    override val amount: Int = ref.quantity()
 
-    override fun snapshot(): ItemStackConfig {
-        TODO("Not yet implemented")
+    override fun snapshot(): ItemStackSnapshot {
+        return ref.asImmutable().wrap()
     }
 
     override fun data(): DataComponentMap<ItemStack> = componentMap

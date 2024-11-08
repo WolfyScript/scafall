61067b9947030e8eeb391b794c5229f3a4e5242f
WolfyScript <wolfyscript@gmail.com>
2024-11-08 23:01:18 +0100

    Add ItemStackSnapshot wrapper ext (Sponge)

--- sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/SpongeWrapper.kt
+++ sponge/src/main/kotlin/com/wolfyscript/scafall/sponge/api/wrappers/SpongeWrapper.kt
@@ -1,10 +1,16 @@
 package com.wolfyscript.scafall.sponge.api.wrappers
 
 import com.wolfyscript.scafall.sponge.api.wrappers.world.items.ItemStackWrapper
+import com.wolfyscript.scafall.sponge.api.wrappers.world.items.SpongeItemStackSnapshot
 import org.spongepowered.api.item.inventory.ItemStack
+import org.spongepowered.api.item.inventory.ItemStackSnapshot
 
 fun ItemStack.wrap() : com.wolfyscript.scafall.wrappers.world.items.ItemStack {
     return ItemStackWrapper(this)
+}
+
+fun ItemStackSnapshot.wrap() : com.wolfyscript.scafall.wrappers.world.items.ItemStackSnapshot {
+    return SpongeItemStackSnapshot(this)
 }
 
 fun com.wolfyscript.scafall.wrappers.world.items.ItemStack.unwrap(): ItemStack {

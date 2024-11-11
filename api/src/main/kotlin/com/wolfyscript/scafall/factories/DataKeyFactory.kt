b24ed894efc44436a74bf05ff5e3c4bfb5996f71
WolfyScript <wolfyscript@gmail.com>
2024-11-11 19:21:28 +0100

    Implement DataKeyFactory

--- api/src/main/kotlin/com/wolfyscript/scafall/factories/DataKeyFactory.kt
+++ api/src/main/kotlin/com/wolfyscript/scafall/factories/DataKeyFactory.kt
@@ -3,9 +3,10 @@ package com.wolfyscript.scafall.factories
 import com.wolfyscript.scafall.data.DataHolder
 import com.wolfyscript.scafall.data.DataKey
 import com.wolfyscript.scafall.identifier.Key
+import kotlin.reflect.KClass
 
 interface DataKeyFactory {
 
-    fun <T : Any, H: DataHolder<*, *>> create(key: Key): DataKey<T, H>
+    fun <T : Any, H: DataHolder<*, *>> create(type: KClass<T>, key: Key): DataKey<T, H>
 
 }
--- common/src/main/kotlin/com/wolfyscript/scafall/common/api/factories/CommonFactories.kt
+++ common/src/main/kotlin/com/wolfyscript/scafall/common/api/factories/CommonFactories.kt
@@ -1,11 +1,14 @@
 package com.wolfyscript.scafall.common.api.factories
 
+import com.wolfyscript.scafall.factories.DataKeyFactory
 import com.wolfyscript.scafall.factories.Factories
 import com.wolfyscript.scafall.factories.IdentifierFactory
 
 abstract class CommonFactories : Factories {
 
     override val identifierFactory: IdentifierFactory = IdentifierFactoryImpl()
+
+    override val dataKeyFactory: DataKeyFactory = DataKeyFactoryImpl()
 
     abstract fun init()
 

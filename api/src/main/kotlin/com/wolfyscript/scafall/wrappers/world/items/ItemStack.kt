8e4bfc266336f5309b8b55ea04bc617e2fabdb75
WolfyScript <wolfyscript@gmail.com>
2024-11-21 18:59:59 +0100

    Provide easier ItemStack creation

--- api/src/main/kotlin/com/wolfyscript/scafall/wrappers/world/items/ItemStack.kt
+++ api/src/main/kotlin/com/wolfyscript/scafall/wrappers/world/items/ItemStack.kt
@@ -17,8 +17,10 @@
  */
 package com.wolfyscript.scafall.wrappers.world.items
 
+import com.wolfyscript.scafall.ScafallProvider
 import com.wolfyscript.scafall.data.DataComponentMap
 import com.wolfyscript.scafall.data.DataHolder
+import com.wolfyscript.scafall.identifier.Key
 
 /**
  * A mutable version of the ItemStack.
@@ -27,6 +29,18 @@ import com.wolfyscript.scafall.data.DataHolder
  * @see ItemStackSnapshot An immutable snapshot of the ItemStack
  */
 interface ItemStack : DataHolder.Mutable<ItemStack>, ItemStackLike<ItemStack, DataComponentMap.Mutable<ItemStack>> {
+
+    companion object {
+
+        fun of(itemType: Key): ItemStack {
+            return ScafallProvider.get().factories.itemsFactory.createStack(itemType)
+        }
+
+        fun of(mcItemType: String) : ItemStack {
+            return of(Key.key(Key.MINECRAFT_NAMESPACE, mcItemType))
+        }
+
+    }
 
     /**
      * Creates a snapshot of the whole ItemStack

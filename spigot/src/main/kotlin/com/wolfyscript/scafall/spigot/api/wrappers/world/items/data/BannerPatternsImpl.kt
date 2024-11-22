4212c2ccdeaef9a67c31b5b26fcc827ff0886611
WolfyScript <wolfyscript@gmail.com>
2024-11-22 19:50:58 +0100

    Use common BannerPatterns implementation

--- spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/wrappers/world/items/data/BannerPatternsImpl.kt
+++ spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/wrappers/world/items/data/BannerPatternsImpl.kt
@@ -1,37 +1,30 @@
 package com.wolfyscript.scafall.spigot.api.wrappers.world.items.data
 
-import com.wolfyscript.scafall.identifier.Key
+import com.wolfyscript.scafall.spigot.api.data.ItemMetaDataKeyConverter
+import com.wolfyscript.scafall.spigot.api.identifiers.bukkit
+import com.wolfyscript.scafall.spigot.api.wrappers.world.items.toBukkit
 import com.wolfyscript.scafall.spigot.api.wrappers.world.items.toWrapper
-import com.wolfyscript.scafall.wrappers.world.items.DyeColor
+import com.wolfyscript.scafall.toAPI
 import com.wolfyscript.scafall.wrappers.world.items.data.BannerPatterns
+import org.bukkit.Registry
 import org.bukkit.block.banner.Pattern
+import org.bukkit.block.banner.PatternType
 import org.bukkit.inventory.meta.BannerMeta
-import java.util.*
 
 internal val bannerPatternsItemMetaConverter = ItemMetaDataKeyConverter<BannerPatterns>(
     {
-        if (this is BannerMeta) BannerPatternsImpl(patterns) else BannerPatternsImpl(emptyList())
+        if (this is BannerMeta) {
+            BannerPatterns(
+                patterns.map {
+                    BannerPatterns.Layer(it.pattern.key.toAPI(), it.color.toWrapper())
+                }
+            )
+        } else BannerPatterns(emptyList())
     },
     { bannerPatterns ->
-        if (this is BannerMeta && bannerPatterns is BannerPatternsImpl) {
-            patterns = bannerPatterns.layers.map { it.toBukkit() }
+        if (this is BannerMeta) {
+            patterns = bannerPatterns.layers.map {
+                Pattern(it.color.toBukkit(), Registry.BANNER_PATTERN.get(it.shape.bukkit()) ?: PatternType.BASE)
+            }
         }
     })
-
-class BannerPatternsImpl(layers: List<Pattern>) : BannerPatterns {
-
-    private val layerWrappers: List<Layer> = layers.map { Layer(it) }
-
-    override val layers: List<Layer> = layerWrappers
-
-    class Layer(private val pattern: Pattern) : BannerPatterns.Layer {
-
-        fun toBukkit(): Pattern {
-            return pattern
-        }
-
-        override val shape: Key = Key.Companion.key(Key.MINECRAFT_NAMESPACE, pattern.pattern.name.lowercase(Locale.getDefault()))
-        override val color: DyeColor = pattern.color.toWrapper()
-    }
-
-}

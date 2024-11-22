dc954c3f1006b32a763ec6e883fe72b2ead8b18c
WolfyScript <wolfyscript@gmail.com>
2024-11-22 19:52:19 +0100

    Use Color wrapper in Fireworks

--- api/src/main/kotlin/com/wolfyscript/scafall/wrappers/world/items/data/Fireworks.kt
+++ api/src/main/kotlin/com/wolfyscript/scafall/wrappers/world/items/data/Fireworks.kt
@@ -18,19 +18,20 @@
 package com.wolfyscript.scafall.wrappers.world.items.data
 
 import com.wolfyscript.scafall.identifier.Key
+import com.wolfyscript.scafall.wrappers.world.Color
 
 interface Fireworks {
 
     val explosions: List<FireworkExplosion>
-    val flightDuration: Byte
+    val flightDuration: Int
 
 }
 
 interface FireworkExplosion {
 
     val shape: Key
-    val colors: List<DyedColor>
-    val fadeColors: List<DyedColor>
+    val colors: List<Color>
+    val fadeColors: List<Color>
     val trail: Boolean
     val twinkle: Boolean
 

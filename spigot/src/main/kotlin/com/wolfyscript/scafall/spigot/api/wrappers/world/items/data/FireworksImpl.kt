d6a85fa0a39e2cc1668424bf2c79c50bf74bbbf1
WolfyScript <wolfyscript@gmail.com>
2024-11-22 19:52:49 +0100

    Implement Fireworks (Spigot)

--- spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/wrappers/world/items/data/FireworksImpl.kt
+++ spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/api/wrappers/world/items/data/FireworksImpl.kt
@@ -1,25 +1,101 @@
 package com.wolfyscript.scafall.spigot.api.wrappers.world.items.data
 
 import com.wolfyscript.scafall.identifier.Key
-import com.wolfyscript.scafall.wrappers.world.items.data.DyedColor
+import com.wolfyscript.scafall.spigot.api.data.ItemMetaDataKeyConverter
+import com.wolfyscript.scafall.spigot.api.wrappers.unwrap
+import com.wolfyscript.scafall.spigot.api.wrappers.wrap
+import com.wolfyscript.scafall.wrappers.world.Color
 import com.wolfyscript.scafall.wrappers.world.items.data.FireworkExplosion
 import com.wolfyscript.scafall.wrappers.world.items.data.Fireworks
+import org.bukkit.FireworkEffect
+import org.bukkit.inventory.meta.FireworkEffectMeta
+import org.bukkit.inventory.meta.FireworkMeta
 
 internal val fireworksItemMetaConverter =
-    ItemMetaDataKeyConverter<Fireworks>({ TODO("Not yet implemented") }, { TODO("Not yet implemented") })
+    ItemMetaDataKeyConverter<Fireworks>({
+        if (this is FireworkMeta) {
+            return@ItemMetaDataKeyConverter FireworksImpl(
+                effects.map { effect ->
+                    FireworkExplosionImpl(
+                        shape = Key.key(
+                            Key.MINECRAFT_NAMESPACE, when (effect.type) {
+                                FireworkEffect.Type.BALL -> "ball"
+                                FireworkEffect.Type.BALL_LARGE -> "large_ball"
+                                FireworkEffect.Type.STAR -> "star"
+                                FireworkEffect.Type.BURST -> "burst"
+                                FireworkEffect.Type.CREEPER -> "creeper"
+                            }
+                        ),
+                        colors = effect.colors.map { it.wrap() },
+                        fadeColors = emptyList(),
+                        trail = effect.hasTrail(),
+                        twinkle = effect.hasFlicker()
+                    )
+                },
+                flightDuration = this.power
+            )
+        }
+
+        null
+    }, {
+        if (this is FireworkMeta) {
+            clearEffects() // Replace the explosions
+            addEffects(
+                it.explosions.map { effect ->
+                    FireworkEffect.builder()
+                        .trail(effect.trail)
+                        .flicker(effect.twinkle)
+                        .withColor(effect.colors.map { it.unwrap() })
+                        .withFade(effect.fadeColors.map { it.unwrap() })
+                        .build()
+                }
+            )
+            power = it.flightDuration
+        }
+    })
 
 internal val fireworkExplosionItemMetaConverter =
-    ItemMetaDataKeyConverter<FireworkExplosion>({ TODO("Not yet implemented") }, { TODO("Not yet implemented") })
+    ItemMetaDataKeyConverter<FireworkExplosion>({
+        if (this is FireworkEffectMeta && hasEffect()) {
+            return@ItemMetaDataKeyConverter effect?.let { effect ->
+                FireworkExplosionImpl(
+                    shape = Key.key(
+                        Key.MINECRAFT_NAMESPACE, when (effect.type) {
+                            FireworkEffect.Type.BALL -> "ball"
+                            FireworkEffect.Type.BALL_LARGE -> "large_ball"
+                            FireworkEffect.Type.STAR -> "star"
+                            FireworkEffect.Type.BURST -> "burst"
+                            FireworkEffect.Type.CREEPER -> "creeper"
+                        }
+                    ),
+                    colors = effect.colors.map { it.wrap() },
+                    fadeColors = effect.fadeColors.map { it.wrap() },
+                    trail = effect.hasTrail(),
+                    twinkle = effect.hasFlicker()
+                )
+            }
+        }
+        null
+    }, {
+        if (this is FireworkEffectMeta) {
+            effect = FireworkEffect.builder()
+                .trail(it.trail)
+                .flicker(it.twinkle)
+                .withColor(it.colors.map { it.unwrap() })
+                .withFade(it.fadeColors.map { it.unwrap() })
+                .build()
+        }
+    })
 
 data class FireworksImpl(
     override val explosions: List<FireworkExplosion>,
-    override val flightDuration: Byte
+    override val flightDuration: Int,
 ) : Fireworks
 
 data class FireworkExplosionImpl(
     override val shape: Key,
-    override val colors: List<DyedColor>,
-    override val fadeColors: List<DyedColor>,
+    override val colors: List<Color>,
+    override val fadeColors: List<Color>,
     override val trail: Boolean,
-    override val twinkle: Boolean
+    override val twinkle: Boolean,
 ) : FireworkExplosion

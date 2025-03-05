43adae6f6b503143c18a3544cab96eaee23379c9
WolfyScript <wolfyscript@gmail.com>
2025-03-05 22:23:28 +0100

    impl: rework attribute modifier data component structure

---
+++ api/src/main/kotlin/com/wolfyscript/scafall/wrappers/world/attribute/ModifierOperation.kt
@@ -0,0 +1,7 @@
+package com.wolfyscript.scafall.wrappers.world.attribute
+
+enum class ModifierOperation(val id: String) {
+    ADD_VALUE("add_value"),
+    ADD_MULTIPLIED_BASE("add_multiplied_base"),
+    ADD_MULTIPLIED_TOTAL("add_multiplied_total"), ;
+}
--- api/src/main/kotlin/com/wolfyscript/scafall/wrappers/world/items/data/AttributeModifiers.kt
+++ api/src/main/kotlin/com/wolfyscript/scafall/wrappers/world/items/data/AttributeModifiers.kt
@@ -18,11 +18,18 @@
 package com.wolfyscript.scafall.wrappers.world.items.data
 
 import com.wolfyscript.scafall.identifier.Key
+import com.wolfyscript.scafall.wrappers.world.attribute.Attribute
+import com.wolfyscript.scafall.wrappers.world.attribute.AttributeModifier
 
 data class AttributeModifiers(
-    val modifiers: List<Modifier>,
+    val modifiers: List<Entry>,
     override val showInTooltip: Boolean
 ) : TooltipApplicable {
+
+    data class Entry(
+        val attribute: Attribute,
+        val modifier: AttributeModifier
+    )
 
     data class Modifier(
         val type: Key,

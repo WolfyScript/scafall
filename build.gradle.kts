368ef4ece2d7293642dc4e364ce599abc9f75bc6
WolfyScript <wolfyscript@gmail.com>
2025-04-21 21:43:40 +0200

    build: update all mentions of jdk 17 to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ tasks {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 artifactory {
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -18,15 +18,15 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 kotlin {
     target {
         compilations.configureEach {
             kotlinOptions {
-                jvmTarget = "17"
+                jvmTarget = "21"
             }
         }
     }

79b648011ca0dad4a5d1d91950c327b4e47c54c3
WolfyScript <wolfyscript@gmail.com>
2025-06-11 16:56:40 +0200

    impl(common): immutable ItemStackRefImpl

---
+++ common/src/main/kotlin/com/wolfyscript/scafall/common/api/items/ItemStackRefImpl.kt
@@ -0,0 +1,45 @@
+package com.wolfyscript.scafall.common.api.items
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.wolfyscript.scafall.items.ItemStackIdentifier
+import com.wolfyscript.scafall.items.ItemStackRef
+import com.wolfyscript.scafall.wrappers.utils.unwrap
+import com.wolfyscript.scafall.wrappers.utils.wrap
+import com.wolfyscript.scafall.wrappers.world.items.ItemStack
+
+/**
+ * Constructs a new instance of a [ItemStackRef].
+ */
+class ItemStackRefImpl(
+    override val amount: Int,
+    override val identifier: ItemStackIdentifier,
+) : ItemStackRef {
+
+    @get:JsonIgnore
+    override val originalStack: ItemStack? by lazy { identifier.create() }
+
+    override fun swapParser(parser: ItemStackIdentifier.Parser<*>): Result<ItemStackRef> {
+        if (originalStack == null) {
+            return Result.failure(IllegalStateException("ItemStackRef has no ItemStack to parse!"))
+        }
+        val parsed = parser.from(originalStack!!)
+        if (parsed == null) {
+            return Result.failure(IllegalStateException("Failed to swap to parser $parser! Emtpy parse result!"))
+        }
+        return Result.success(ItemStackRefImpl(amount, parsed))
+    }
+
+    override fun matches(
+        stack: ItemStack,
+        matchTags: Boolean,
+    ): Boolean {
+        return  identifier.matches(stack, matchTags)
+    }
+
+    override fun create(): ItemStack {
+        val stack = identifier.create().unwrap()
+        stack.count = amount
+        return stack.wrap()
+    }
+
+}

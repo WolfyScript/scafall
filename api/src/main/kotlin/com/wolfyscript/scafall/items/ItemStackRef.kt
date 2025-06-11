4ac842e5bf318211bbbf13916673e7d7f87d8dd0
WolfyScript <wolfyscript@gmail.com>
2025-06-11 16:55:47 +0200

    api: improve ItemStackRef
    * do not store parser, that one is available via identifier
    * return Result for swapParser for better error handling

--- api/src/main/kotlin/com/wolfyscript/scafall/items/ItemStackRef.kt
+++ api/src/main/kotlin/com/wolfyscript/scafall/items/ItemStackRef.kt
@@ -11,25 +11,20 @@ interface ItemStackRef {
      * The stack this [ItemStackRef] was created from and the [ItemStackIdentifier] parsed. Otherwise, referenced stack; If neither is available, AIR.
      */
     @get:JsonIgnore
-    val originalStack: ItemStack
+    val originalStack: ItemStack?
 
     /**
      * The identifier used to construct this ref, or the identifier parsed from the [originalStack]
      * Null if neither is available.
      */
-    val identifier: ItemStackIdentifier?
-
-    /**
-     * Parser used to parse the [identifier] from an [ItemStack]
-     */
-    val parser: ItemStackIdentifier.Parser<*>?
+    val identifier: ItemStackIdentifier
 
     /**
      * Swaps the parser used to parse the [identifier] from an [ItemStack]
      *
      * @return a new [ItemStackRef] with the new parser
      */
-    fun swapParser(parser: ItemStackIdentifier.Parser<*>): ItemStackRef
+    fun swapParser(parser: ItemStackIdentifier.Parser<*>): Result<ItemStackRef>
 
     fun matches(stack: ItemStack, matchTags: Boolean): Boolean
 

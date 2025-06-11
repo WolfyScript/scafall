20b944f7f414d3a5b26c48cb925bc3e3691ea2e7
WolfyScript <wolfyscript@gmail.com>
2025-06-11 16:57:01 +0200

    impl(common): add VanillaItemStackIdentifier

---
+++ common/src/main/kotlin/com/wolfyscript/scafall/common/api/items/VanillaItemStackIdentifier.kt
@@ -0,0 +1,45 @@
+package com.wolfyscript.scafall.common.api.items
+
+import com.wolfyscript.scafall.items.ItemStackIdentifier
+import com.wolfyscript.scafall.wrappers.utils.unwrap
+import com.wolfyscript.scafall.wrappers.utils.wrap
+import com.wolfyscript.scafall.wrappers.world.items.ItemStack
+
+class VanillaItemStackIdentifier(val stack: ItemStack) : ItemStackIdentifier {
+
+    override val parser: ItemStackIdentifier.Parser<*>
+        get() = TODO("Not yet implemented")
+
+    override fun matches(
+        stack: ItemStack,
+        matchTags: Boolean,
+    ): Boolean {
+        val other = stack.unwrap()
+        val thisStack = this.stack.unwrap()
+        if (other.count != thisStack.count) {
+            return false
+        }
+        if (thisStack == other) {
+            return true // Same instance of stacks, so they must be equal!
+        }
+        if (matchTags) {
+            return net.minecraft.world.item.ItemStack.isSameItemSameComponents(thisStack, other)
+        }
+        return net.minecraft.world.item.ItemStack.isSameItem(thisStack, other)
+    }
+
+    override fun create(): ItemStack {
+        return stack.unwrap().copy().wrap()
+    }
+
+    class Parser : ItemStackIdentifier.Parser<VanillaItemStackIdentifier> {
+
+        override val priority: Int = 0
+
+        override fun from(stack: ItemStack): VanillaItemStackIdentifier? {
+            return VanillaItemStackIdentifier(stack)
+        }
+
+    }
+
+}

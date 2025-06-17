7503b6201ec1641ba9b7180bf51c3975c62f8152
WolfyScript <wolfyscript@gmail.com>
2025-06-17 22:50:23 +0200

    api: add ItemStackIdentifiers.Parsers keys

--- api/src/main/kotlin/com/wolfyscript/scafall/items/ItemStackIdentifiers.kt
+++ api/src/main/kotlin/com/wolfyscript/scafall/items/ItemStackIdentifiers.kt
@@ -1,19 +1,32 @@
 package com.wolfyscript.scafall.items
 
 import com.wolfyscript.scafall.identifier.Key
-import com.wolfyscript.scafall.registry.ValueKey
 import com.wolfyscript.scafall.registry.ValueReference
 import com.wolfyscript.scafall.registry.ScafallRegistryTypes
+import com.wolfyscript.scafall.registry.referenced
 
 /**
  * Contains the [ItemStackIdentifiers][ItemStackIdentifier] that are available across all platforms.
  */
 object ItemStackIdentifiers {
 
-    val vanilla = create("vanilla")
+    val vanilla = create<Class<VanillaItemStackIdentifier>>("vanilla")
 
-    private fun create(key: String) : ValueReference<Class<out ItemStackIdentifier>> {
-        return ValueKey.of(ScafallRegistryTypes.itemStackIdentifiers.key, Key.key(Key.SCAFFOLDING_NAMESPACE, key)).reference()
+    private inline fun <reified T: Class<out ItemStackIdentifier>> create(key: String) : ValueReference<Class<out ItemStackIdentifier>, T> {
+        return ScafallRegistryTypes.itemStackIdentifiers.key.referenced<Class<out ItemStackIdentifier>, T>(Key.key(Key.SCAFFOLDING_NAMESPACE, key)).reference()
+    }
+
+    /**
+     * The parsers for the ItemStackIdentifiers
+     */
+    object Parsers {
+
+        val vanilla = create<VanillaItemStackIdentifier.Parser>("vanilla")
+
+        private inline fun <reified T: ItemStackIdentifier.Parser<*>> create(key: String) : ValueReference<ItemStackIdentifier.Parser<*>, T> {
+            return ScafallRegistryTypes.itemStackIdentifierParsers.key.referenced<ItemStackIdentifier.Parser<*>, T>(Key.key(Key.SCAFFOLDING_NAMESPACE, key)).reference()
+        }
+
     }
 
 }

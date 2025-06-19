a2009500e52cd1904e5d5da36003be98286d7791
WolfyScript <wolfyscript@gmail.com>
2025-06-19 17:16:13 +0200

    impl: add equal and hashCode to position wrappers

--- common/src/main/kotlin/com/wolfyscript/scafall/common/api/wrappers/world/ScafallPos.kt
+++ common/src/main/kotlin/com/wolfyscript/scafall/common/api/wrappers/world/ScafallPos.kt
@@ -14,6 +14,17 @@ class ScafallBlockPosCommon(val blockPos: BlockPos) : ScafallBlockPos {
     override val y: Int = blockPos.y
     override val z: Int = blockPos.z
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ScafallBlockPosCommon) return false
+        if (blockPos != other.blockPos) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return blockPos.hashCode()
+    }
+
 }
 
 class ScafallPrecisePosCommon(val pos: Vec3) : ScafallPrecisePos {
@@ -22,14 +33,61 @@ class ScafallPrecisePosCommon(val pos: Vec3) : ScafallPrecisePos {
     override val y: Double = pos.y
     override val z: Double = pos.z
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ScafallPrecisePosCommon) return false
+
+        if (pos != other.pos) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return pos.hashCode()
+    }
+
 }
 
 class ScafallGlobalBlockPosCommon(
     override val dimension: Key,
     override val blockPos: ScafallBlockPos,
-) : ScafallGlobalBlockPos
+) : ScafallGlobalBlockPos {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ScafallGlobalBlockPosCommon) return false
+
+        if (dimension != other.dimension) return false
+        if (blockPos != other.blockPos) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = dimension.hashCode()
+        result = 31 * result + blockPos.hashCode()
+        return result
+    }
+}
 
 class ScafallGlobalPrecisePosCommon(
     override val dimension: Key,
     override val pos: ScafallPrecisePos,
-) : ScafallGlobalPrecisePos
+) : ScafallGlobalPrecisePos {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ScafallGlobalPrecisePosCommon) return false
+
+        if (dimension != other.dimension) return false
+        if (pos != other.pos) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = dimension.hashCode()
+        result = 31 * result + pos.hashCode()
+        return result
+    }
+}

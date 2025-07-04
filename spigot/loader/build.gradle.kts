72cc0b5269e9569219bd84e2d78c96f1bb43b196
WolfyScript <wolfyscript@gmail.com>
2025-07-04 19:09:53 +0200

    update spigot test servers to 1.21.7

--- spigot/loader/build.gradle.kts
+++ spigot/loader/build.gradle.kts
@@ -73,7 +73,7 @@ minecraftServers {
         // Scaffolding will only support 1.21+
         register("spigot_1_21") {
             destFileName.set("scafall.jar")
-            version.set("1.21.5")
+            version.set("1.21.7")
             type.set("SPIGOT")
             imageVersion.set("java21")
             ports.set(setOf(debugPortMapping, "25565:25565"))
@@ -81,7 +81,7 @@ minecraftServers {
         // Paper test servers
         register("paper_1_21") {
             destFileName.set("scafall.jar")
-            version.set("1.21.5")
+            version.set("1.21.7")
             type.set("PAPER")
             imageVersion.set("java21")
             ports.set(setOf(debugPortMapping, "25566:25565"))

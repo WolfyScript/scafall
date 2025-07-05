f6e60477ef614d4586d8e942ed9f3dc51a5c9feb
WolfyScript <wolfyscript@gmail.com>
2025-07-05 19:29:22 +0200

    feat: add util to run function on specific platforms

--- common/src/main/kotlin/com/wolfyscript/scafall/common/api/platform/CommonPlatformManager.kt
+++ common/src/main/kotlin/com/wolfyscript/scafall/common/api/platform/CommonPlatformManager.kt
@@ -1,9 +1,11 @@
 package com.wolfyscript.scafall.common.api.platform
 
+import com.wolfyscript.scafall.ScafallProvider
 import com.wolfyscript.scafall.identifier.Key
 import com.wolfyscript.scafall.loader.InnerJarClassloader
 import com.wolfyscript.scafall.loader.module.Module
 import com.wolfyscript.scafall.platform.PlatformManager
+import com.wolfyscript.scafall.platform.PlatformType
 
 abstract class CommonPlatformManager(val scafallClassLoader: ClassLoader) : PlatformManager {
 
@@ -39,5 +41,25 @@ abstract class CommonPlatformManager(val scafallClassLoader: ClassLoader) : Plat
         return moduleEntry.bridge as T
     }
 
+}
 
+/**
+ * Runs the given function if the current [PlatformManager.platformType] is equal to the specified [platform]
+ */
+fun ifPlatform(platform: PlatformType, fn: () -> Unit) {
+    if (ScafallProvider.get().platformManager.platformType == platform) {
+        fn()
+    }
+}
+
+/**
+ * Runs the given [then] function if the current [PlatformManager.platformType] is equal to the specified [platform].
+ * Otherwise, runs the [orElse] function.
+ */
+fun ifPlatformOrElse(platform: PlatformType, then: () -> Unit, orElse: () -> Unit) {
+    if (ScafallProvider.get().platformManager.platformType == platform) {
+        then()
+        return
+    }
+    orElse()
 }

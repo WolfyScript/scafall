13eed9090898c8828f77179e9f78d60134cd1f15
WolfyScript <wolfyscript@gmail.com>
2025-07-05 19:04:40 +0200

    refactor: remove spigot persistent storage utils

--- spigot/src/main/kotlin/com/wolfyscript/scafall/spigot/platform/persistent/events/BlockStorageEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.wolfyscript.scafall.spigot.platform.persistent.events
-
-import com.wolfyscript.scafall.spigot.platform.persistent.world.BlockStorage
-
-interface BlockStorageEvent {
-    val storage: BlockStorage
-}

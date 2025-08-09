119b469947ce743806fc2eac28ef422bd5dd7957
WolfyScript <wolfyscript@gmail.com>
2025-08-09 15:32:54 +0200

    build: upgrade to kotlin 2.2.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
 
     implementation(libs.plugins.devtools.docker.run.text())
     implementation(libs.plugins.devtools.docker.minecraft.text())
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
 }
 
 java {

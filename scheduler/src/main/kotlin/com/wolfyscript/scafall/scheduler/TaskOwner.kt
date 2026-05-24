05d0b3111dc8b179ca0a92a78c4cc18d2138f9f1
WolfyScript <wolfyscript@gmail.com>
2026-05-24 11:08:30 +0200

    feat(scheduler): add simple task owner

--- scheduler/src/main/kotlin/com/wolfyscript/scafall/scheduler/TaskOwner.kt
+++ scheduler/src/main/kotlin/com/wolfyscript/scafall/scheduler/TaskOwner.kt
@@ -1,9 +1,24 @@
 package com.wolfyscript.scafall.scheduler
 
+import java.util.UUID
+
 /**
  * Identifies the owner of a task.
  *
  * Used to determine who is responsible for a task, and allows to cancel tasks of a specific owner, for example if that owner is no longer available.
  * Like a mod or plugin that created a task gets disabled or unloaded, then all tasks created by that mod or plugin should be cancelled.
  */
-interface TaskOwner
+interface TaskOwner {
+
+    companion object {
+
+        /**
+         * Creates a new simple unique [TaskOwner]
+         */
+        fun createSimple(): TaskOwner {
+            return SimpleTaskOwner(UUID.randomUUID())
+        }
+
+    }
+
+}
